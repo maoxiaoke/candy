d8a7d914d3f3e3c6706cbd0600c6edc3a2f5ff4b
maoxiaoke <maoxiaoke@outlook.com>
2024-08-08 11:20:26 +0800

    add support

--- src/components/ui/switch/index.jsx
+++ src/components/ui/switch/index.jsx
@@ -3,7 +3,7 @@ import * as RadixSwitch from '@radix-ui/react-switch';
 import './styles.css';
 
 const Switch = ({ label, defaultChecked, onCheckedChange, value }) => (
-  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', fontWeight: 500 }}>
     <label className="Label" htmlFor="airplane-mode" style={{ paddingRight: 15 }}>
       {label}
     </label>
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -4,6 +4,10 @@ import { Switch } from '../../components/ui/switch'
 import './Popup.css';
 import { getSettings, defaultSettings } from '../../utils/index';
 
+const openUrl = (url) => {
+  chrome.tabs.create({ url });
+}
+
 const Popup = () => {
   const [settings, setSettings] = React.useState(defaultSettings);
 
@@ -42,10 +46,32 @@ const Popup = () => {
   return (
     <div className="popup">
       <form>
+
         <Switch label="Rename image after loaded" onCheckedChange={onRenameChange} value={settings?.renameAfterDownload} />
+
+        <label style={{
+          fontSize: '12px',
+          display: 'inline-block',
+          marginTop: '2px',
+          lineHeight: '14px',
+          color: '#666'
+        }}>
+          When this option is turned off, the download dialog will only appear after GPT has finished returning the response, which sometimes may not be immediate.
+        </label>
         
         {/* <div className="divider"></div> */}
         {/* <Switch label="Get response when context menu shown" onCheckedChange={onContextMenuChange} value={settings?.getResponseWhenContextMenuShown} /> */}
+
+        <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+          <a href="https://buymeacoffee.com/nazha" onClick={(evt) => {
+            evt.preventDefault();
+            openUrl('https://buymeacoffee.com/nazha')
+          }}>Support</a>
+          <a href="https://x.com/xiaokedada" onClick={(evt) => {
+            evt.preventDefault();
+            openUrl('https://x.com/xiaokedada')
+          }}>Author on X</a>
+        </div>
       </form>
     </div>
   );
